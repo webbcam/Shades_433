af15ee2e60ca56719194f00e3bd25f0c7a0d591e
webbcam <webb.c.92@gmail.com>
2015-12-25 00:18:11 -0500

    added EEPROM storage for 3 signals

--- Shades_433.ino
+++ Shades_433.ino
@@ -9,8 +9,8 @@
 #define SPEED 15
 #define INTERVAL 5
 #define SERVO_PIN 9
-#define BUTTON_PIN 3
-#define LED_PIN 12
+#define BUTTON_PIN 4
+#define LED_PIN 8
 #define TIMEOUT 3000
 
 Servo myServo;
@@ -18,8 +18,10 @@ RCSwitch mySwitch = RCSwitch();
 
 //  get shade ID
 byte shade_ID = EEPROM.read(0);
-//  get remote ID
-unsigned int remote_ID = ( EEPROM.read(1) << 8) | EEPROM.read(2);
+
+unsigned int remote_ID[3];
+byte EEPROM_ptr = EEPROM.read(1); // used as EEPROM stack ptr
+byte ID_ptr = EEPROM_ptr/2 - 1;   // used to traverse thru remote_ID array
 //  set initial shades postion
 int pos = OFFSET;
 
@@ -28,12 +30,24 @@ void setup() {
   Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
-  mySwitch.enableReceive(0);  //  Receiver on interrupt 0 => pin #2
-  //attachInterrupt(1, set_remote_ID, FALLING);  // Button on interrupt 1 => pin #3
+  pinMode(3, INPUT);
+  
+  //  get remote IDs (x3)
+  remote_ID[0] = ( EEPROM.read(2) << 8) | EEPROM.read(3);
+  remote_ID[1] = ( EEPROM.read(4) << 8) | EEPROM.read(5);
+  remote_ID[2] = ( EEPROM.read(6) << 8) | EEPROM.read(7);
+  
+  mySwitch.enableReceive(1);  //  Receiver on interrupt 1 => pin #3
   Serial.print("shade_ID is: ");
   Serial.println(shade_ID);
-  Serial.print("remote_ID is: ");
-  Serial.println(remote_ID);
+  for (int i = 0; i < 3; i++) {
+    Serial.print("remote_ID");
+    Serial.print(i + 1);
+    Serial.print(" is: ");
+    Serial.println(remote_ID[i]);
+  }
+  Serial.print("EEPROM_ptr is: ");
+  Serial.println(EEPROM_ptr);
 }
 
 void loop() {
@@ -69,7 +83,7 @@ void loop() {
           adjust_shade(deg);
         }
         //  check for signal from remote
-      } else if (load == remote_ID) {
+      } else if (load == remote_ID[0] || load == remote_ID[1] || load == remote_ID[2]) {
         Serial.println("RECIEVED SIGNAL FROM REMOTE!!!");
         if (postamble == POST_ON)
           adjust_shade(0);    // open shades
@@ -102,9 +116,9 @@ void set_shade_ID(byte id) {
   EEPROM.update(0, shade_ID);
   //  signal that the new shade_ID has been successfully set
   for (int i = 0; i < 4; i++) {
-    digitalWrite(12, HIGH);
+    digitalWrite(LED_PIN, HIGH);
     delay(100);
-    digitalWrite(12, LOW);
+    digitalWrite(LED _PIN, LOW);
     delay(100);
   }
 }
@@ -116,27 +130,36 @@ void set_remote_ID(void) {
   //  clear any signal stored
   mySwitch.resetAvailable();
   //  signal ready for signal
-  digitalWrite(12, HIGH);
+  digitalWrite(LED_PIN, HIGH);
   timeout = millis() + TIMEOUT;
   while (!mySwitch.available() && millis() < timeout)
     ; // wait until we get a signal
   load = parse_load(mySwitch.getReceivedValue());
   //  if new signal, save it
-  if (remote_ID != load) {
-    remote_ID = load;
+  if (remote_ID[ID_ptr] != load) {
+    remote_ID[ID_ptr] = load;
     msb = (load >> 8) & 0xFF;
     lsb = load & 0xFF;
 
     //  save the new remote_ID (big endian)
-    EEPROM.update(1, msb);
-    EEPROM.update(2, lsb);
+    EEPROM.update(EEPROM_ptr, msb);
+    EEPROM.update(EEPROM_ptr + 1, lsb);
+
   }
+    //  adjust pointers
+    if (EEPROM_ptr != 6)
+      EEPROM_ptr += 2;
+    else
+      EEPROM_ptr = 2;
+    ID_ptr = EEPROM_ptr/2 - 1;
+
+    EEPROM.update(1, EEPROM_ptr);
 
   //  signal that the new remote_ID has been successfully set
   for (int i = 0; i < 4; i++) {
-    digitalWrite(12, HIGH);
+    digitalWrite(LED_PIN, HIGH);
     delay(100);
-    digitalWrite(12, LOW);
+    digitalWrite(LED_PIN, LOW);
     delay(100);
   }
 
---
+++ shades_initialize/shades_initialize.ino
@@ -0,0 +1,23 @@
+/*
+ * Initializes Chip's EEPROM for first use.
+ */
+
+#include <EEPROM.h>
+
+void setup() {
+  pinMode(12, OUTPUT);
+  // put your setup code here, to run once:
+  EEPROM.write(0, 0);   //  set shade_ID to 0
+  EEPROM.write(1, 2);   //  point remote_ID to first open space
+  //  clear out remote_ID
+  for (int i = 2; i < 8; i++) {
+    EEPROM.write(i, 0);
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(12, HIGH);
+
+}
